c8fe356e067f4a768823dfc6adea78cf399798eb
LNIXZ <47589305+LNIXZ@users.noreply.github.com>
2019-02-14 19:14:12 +1100

    Add files via upload

--- Calcutor.py
+++ Calcutor.py
@@ -1,6 +1,8 @@
 
 from fractions import Fraction
 from fractions import gcd
+import math
+
 
 def add(x, y):
    return x + y
@@ -19,31 +21,76 @@ def divide(x, y):
 
 
 
+mode = input("Normal(n) or scientific(s): ")
 
-num1 = int(input("Enter first number: "))
-choice = input("Sign: ")
-num2 = int(input("Enter second number: "))
 
-if choice == '+':
-   print(num1,"+",num2,"=", add(num1,num2))
 
-elif choice == '-':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+if mode == 'n':
+   num1 = int(input("Enter first number: "))
+   choice = input("Sign: ")
+   num2 = int(input("Enter second number: "))
+   if choice == '+':
+      print(num1,"+",num2,"=", add(num1,num2))
 
-elif choice == '*':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+   elif choice == '-':
+      print(num1,"-",num2,"=", subtract(num1,num2))
 
-elif choice == '/':
-   print(num1,"/",num2,"=", divide(num1,num2))
+   elif choice == '*':
+      print(num1,"*",num2,"=", multiply(num1,num2))
 
-elif choice == 'f':
-   i = Fraction(num1, num2)
-   print(i)
+   elif choice == '/':
+      print(num1,"/",num2,"=", divide(num1,num2))
 
-elif choice == 'gcd':
-   print(gcd(num1, num2)) 
+   elif choice == 'f':
+      i = Fraction(num1, num2)
+      print(i)
+
+   elif choice == 'gcd':
+      print("gcd of",num1,"and",num2,"=",gcd(num1, num2)) 
+   else:
+      print("Invalid input")
+
+elif mode == 's':
+ num1 = int(input("Enter first number: "))
+ choice = input("Sign: ")
+
+ if choice == 'surd':
+  def sqrt(n):
+   list = []
+   N = n
+   i = 100
+   I = 0
+   multiplier = []
+ 
+   for p in range(0, 101):
+    list.append(p ** 2)
+ 
+   while i > 0:
+    if n % list[i] == 0:
+     multiplier.append(list[i])
+     n //= list[i]
+     i -= 1
+    else:
+     i -= 1
+   
+   product = multiplier[I]
+   x = I + 1
+
+   while x < len(multiplier):
+    product *= multiplier[x]
+    x += 1
+
+   print("The surd form of the sqrt of", N, "is", int(math.sqrt(product)), "* sqrt of", n, ".")
+
+  n = num1
+  sqrt(n)
+ elif choice == 'n/a':
+   print('done')
+ else:
+   print('test')
+
 else:
-   print("Invalid input")
+   print('n or s not pressed')
 
 
 
